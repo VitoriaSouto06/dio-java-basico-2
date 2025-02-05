0b5fd322450555a87a2588da517e376cb61c85f9
VitoriaSouto06 <soutovitoria06@gmail.com>
2025-02-04 22:05:52 -0300

    desafio finalizado

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero: ");
+        int x = scanner.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int y = scanner.nextInt();
+
+       
+       try{
+        contar(x,y);
+       }
+       catch(Tratativa e){
+        System.out.println("Erro: " + e.getMessage());
+       }
+    }
+    static void  contar(int x, int y) throws Tratativa{
+        if (x > y) {
+            throw new Tratativa("O primeiro parametro nao pode ser maior que o segundo");
+        }
+       
+        int contagem = y - x;
+            for (int c = 0; c < contagem; c++) {
+                System.out.println(c+1);
+            }
+        
+    }
+
+   
+    
+}
+
+class Tratativa extends Exception {
+    public Tratativa(String texto) {
+        super(texto); 
     }
 }
